4d436bf751e12f5d09c98129e359c5cdbd506001
Rakshitha <rakshugatti17@gmail.com>
2026-02-18 09:02:22 +0530

    initial commit

---
+++ module3q1.py
@@ -0,0 +1,7 @@
+text="Hello, World!"
+name="Rakshitha G M"
+age =21
+fav_lang = "Python"
+print(text)
+print("My name is:", name, "My age is:", age, "My favorite programming language is:", fav_lang)
+
---
+++ module3q2.py
@@ -0,0 +1,11 @@
+# taking input as your name
+name = input("Enter your name: ")
+
+# take your birth year
+birth_year = int(input("Enter your birth year: "))
+
+#  TO calculate age
+age = 2026 - birth_year
+
+# print the message
+print("Hello", name + ", you are approximately", age, "years old.")
